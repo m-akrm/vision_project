a78016086ada5fc9c8941c78b482d269ebb72ff5
mahmoud akrm mahmoud mohamed <113924991+m-akrm@users.noreply.github.com>
2023-12-23 00:01:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,55 @@
-import tensorflow as tf 
-print(tf.config.list_physical_devices('GPU'))
+from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow all origins for CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Endpoint to read data from output.txt
+@app.get("/read_data")
+def read_data():
+    try:
+        with open("output.txt", "r") as file:
+            data = file.read()
+        return {"data": data}
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+
+# Endpoint to append data to output.txt
+@app.post("/append_data")
+def append_data(data: str):
+    try:
+        with open("output.txt", "a") as file:
+            file.write(data + "\n")
+        return {"status": "Data appended successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Additional endpoint to upload a file and append its content to output.txt
+@app.post("/upload_file")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        with open("output.txt", "a") as file:
+            file.write(contents.decode("utf-8") + "\n")
+        return {"status": "File content appended successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Endpoint to download the output.txt file
+@app.get("/download_file")
+def download_file():
+    return FileResponse("output.txt", filename="output.txt")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
